831c671fb6eb915e4aed50a4b025064c7c3717ae
Johannes Schultz <sagamusix@users.noreply.github.com>
2024-03-17 19:22:34 +0100

    update fuzzing infrastructure

--- fuzzing/build.sh
+++ fuzzing/build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 cd "${0%/*}"
 cd ..
-CXX=fuzzing/afl/afl-clang-fast++ LD=fuzzing/afl/afl-clang-fast++ MAIN=fuzz.o AFL_HARDEN=1 make -f Makefile.unix clean all
+CXX=fuzzing/afl/afl-clang-fast++ LD=fuzzing/afl/afl-clang-fast++ MAIN_N=fuzz.o AFL_HARDEN=1 make -f Makefile.unix clean all

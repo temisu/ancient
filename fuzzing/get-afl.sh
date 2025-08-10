f2ca579df9a15484234e1facd8b7406006adfd9f
Teemu Suutari <tz@iki.fi>
2025-08-10 17:17:04 +0300

    fixing workflows + rename

--- fuzzing/get-afl.sh
+++ fuzzing/get-afl.sh
@@ -3,7 +3,7 @@ cd "${0%/*}"
 
 AFL_VERSION="$(wget --quiet -O - "https://api.github.com/repos/AFLplusplus/AFLplusplus/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")')"
 AFL_FILENAME="$AFL_VERSION.tar.gz"
-AFL_URL="https://github.com/AFLplusplus/AFLplusplus/archive/$AFL_FILENAME"
+AFL_URL="https://github.com/AFLplusplus/AFLplusplus/refs/tags/$AFL_FILENAME"
 
 rm $AFL_FILENAME
 wget $AFL_URL || exit
